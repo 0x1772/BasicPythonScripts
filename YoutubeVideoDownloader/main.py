74833f2604b8096f727ffedeb71063280e4bd1d1
0x1772 <88681966+FakeSmileVX@users.noreply.github.com>
2022-07-22 18:47:51 +0300

    Create main.py

---
+++ YoutubeVideoDownloader/main.py
@@ -0,0 +1,21 @@
+#Don't forget to install PyTube with ==> pip install PyTube
+#Don't forget to install Tkinter with ==> pip install Tkinter
+
+from pytube import YouTube
+
+#ask for the link from user
+link = input("Enter the link of Video:  ")
+yt = YouTube(link)
+
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+#Getting the highest resolution possible
+ys = yt.streams.get_highest_resolution()
+
+#Starting download
+print("Downloading...")
+ys.download()
+print("Download completed!!")
